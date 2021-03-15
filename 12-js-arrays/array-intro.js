7cdede98ee1b4400c6a9582a0ad621ea8172150b
Stephen Fillers <stephenfillers@gmail.com>
2021-03-14 21:03:58 -0400

    Working through more JavaScript lessons, including object literals.

--- 12-js-arrays/array-intro.js
+++ 12-js-arrays/array-intro.js
@@ -1,1 +1,16 @@
 let days = ["monday", "tuesday", "wednesday"]
+
+let movieLine = ['tom', 'nancy']
+movieLine.push('pablo', 'oliver', 'eva')
+
+const colors = [
+    ['red'],
+    ['orange'],
+    ['yellow'],
+]
+
+const gameBoard = [
+    ['X', 'O', 'X'],
+    ['O', null, 'X'],
+    ['O', 'O', 'X']
+]
---
+++ 13-js-object-literals/script.js
@@ -0,0 +1,44 @@
+const person = {
+    firstName: 'Stephen',
+    lastName: 'Fillers',
+    upvotes: 20,
+    downvotes: 2,
+    netScore: 18,
+}
+
+const kitchenSink = {
+    favNum: 8432,
+    isFunny: true,
+    colors: ['red', 'orange']
+}
+
+const years = {
+    1999: 'Good',
+    2020: 'Bad',
+}
+
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+const midterms = {
+    danielle: 96,
+    thomas: 78
+}
+
+const comments = [
+    {
+        username: 'Tammy',
+        text: 'lolololol',
+        votes: 9
+    },
+    {
+        username: 'FishBoi',
+        text: 'glub glub',
+        votes: 12387
+    }
+]
