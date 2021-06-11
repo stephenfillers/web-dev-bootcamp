b0538512d5bff6e680adf1b8db9bccbad67a6dd7
Stephen Fillers <stephenfillers@gmail.com>
2021-06-10 21:46:27 -0400

    Working through the loops curriculum. This is tough!

---
+++ 14-js-loops/for-loops.js
@@ -0,0 +1,32 @@
+// for loop structure = initialExpression, condition, incrementExpression
+// be sure to set the increment expression to equal something, otherwise it will infinite loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// a loop for every even number up to 20
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// a loop counting down from 100 by 10
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+// multiply to 1000 by factors of 10
+// for (let i = 10; i <= 1000; i *= 10) {
+//     console.log(i);
+// }
+
+// const animals = ['lions', 'tigers', 'bears'];
+
+// looping through an array
+// for (let i = 0; i < animals.length; i++) {
+//     console.log(i, animals[i]);
+// }
+
+// looping through the same array, but in reverse
+// for (let i = animals.length - 1; i >= 0; i--) {
+//     console.log(i, animals[i]);
+// }
