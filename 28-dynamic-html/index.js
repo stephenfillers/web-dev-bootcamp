5c4f76304994d7ece93b6cacf66ab528065b6eda
Stephen Fillers <9887645+stephenfillers@users.noreply.github.com>
2021-07-27 22:56:30 -0400

    Learned additional templating with EJS and Express, partials, and integrating bootstrap into the mix.

--- 28-dynamic-html/index.js
+++ 28-dynamic-html/index.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/r/:sub', (req, res) => {
-    const { sub } = req.params;
-    res.render('subreddit', { sub });
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if(data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit });
+    }
 });
 
-app.get('/cats', (req, res) => {
-    const cats = [
-        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
-    ]
-    res.render('cats', { cats })
-});
+// app.get('/cats', (req, res) => {
+//     const cats = [
+//         'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+//     ]
+//     res.render('cats', { cats })
+// });
 
-app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { rand: num });
-});
+// app.get('/rand', (req, res) => {
+//     const num = Math.floor(Math.random() * 10) + 1;
+//     res.render('random', { rand: num });
+// });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
+app.listen(3000, () => {
+    console.log('Listening on port: 3000')
 });
