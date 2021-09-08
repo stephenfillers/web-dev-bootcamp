ab745f573cb3e3bbde2b54dbdf14df0b4dfe9c25
Stephen Fillers <9887645+stephenfillers@users.noreply.github.com>
2021-09-07 20:51:30 -0400

    Create first data-driven webpage

--- 31-mongoose-express/index.js
+++ 31-mongoose-express/index.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose =  require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongo connection open!")
     })
@@ -15,8 +17,10 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, u
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('Woof!');
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    console.log(products);
+    res.render('products/index', { products });
 })
 
 app.listen(3000, () => {
