3d3f012741d267483103c529e1c05def7934aa9c
Stephen Fillers <9887645+stephenfillers@users.noreply.github.com>
2021-09-07 20:11:09 -0400

    Stage mongo/mongoose/express project

--- 31-mongoose-express/index.js
+++ 31-mongoose-express/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose =  require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo connection open!")
+    })
+    .catch(err => {
+        console.log("Sorry, I've encountered a Mongo error.");
+        console.log(err);
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -9,6 +19,6 @@ app.get('/dog', (req, res) => {
     res.send('Woof!');
 })
 
-app.listen(5500, () => {
+app.listen(3000, () => {
     console.log("App is listening on port 3000!")
 })
